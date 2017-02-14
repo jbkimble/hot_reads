d1c41dab305eab01797270541fe2797ce05da27c
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-13 18:27:13 -0700

    Api endpoint and display view

---
+++ app/controllers/api/v1/urls_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::UrlsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+      url = Url.new(url_params)
+    if url.save
+      render json: url, :only => [:id, :url]
+    else
+    end
+  end
+
+  private
+    def url_params
+      binding.pry
+      params.require(:url).permit(:url)
+    end
+end
