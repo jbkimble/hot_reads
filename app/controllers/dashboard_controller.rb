03ea16fed00a9852eccb3cfebe20db0486439d1a
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-25 12:44:20 -0700

    Improve display info

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,6 @@
 class DashboardController < ApplicationController
   def index
     @count = 0
-    # @urls = Url.where(updated_at: (Time.now - 24.hours)..Time.now)
     @urls = Url.where(updated_at: (Time.now - 24.hours)..Time.now).group('url').limit(10).count
   end
 end
