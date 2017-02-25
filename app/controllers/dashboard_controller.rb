2f34a1a138fe8117c41fb83e17230cc5200f7c75
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-25 12:36:20 -0700

    made sql more specific

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,8 @@
 class DashboardController < ApplicationController
   def index
     @count = 0
-    @urls = Url.where(updated_at: (Time.now - 24.hours)..Time.now)
-    # @urls = Url.where(updated_at: (Time.now - 24.hours)..Time.now).group(:url)
+    # @urls = Url.where(updated_at: (Time.now - 24.hours)..Time.now)
+    @urls = Url.where(updated_at: (Time.now - 24.hours)..Time.now).group('url').limit(10).count
+    binding.pry
   end
 end
