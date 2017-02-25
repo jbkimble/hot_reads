95e93b2d918a1ca1b04da24801c3e0947603b10e
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-25 12:37:12 -0700

    Remove pry

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,5 @@ class DashboardController < ApplicationController
     @count = 0
     # @urls = Url.where(updated_at: (Time.now - 24.hours)..Time.now)
     @urls = Url.where(updated_at: (Time.now - 24.hours)..Time.now).group('url').limit(10).count
-    binding.pry
   end
 end
