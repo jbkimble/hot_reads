7c562a4410f7557fd28091f719100aea6fb5de1c
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-25 12:49:21 -0700

    Fix ordering of results

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class DashboardController < ApplicationController
   def index
     @count = 0
-    @urls = Url.where(updated_at: (Time.now - 24.hours)..Time.now).group('url').limit(10).count
+    urls = Url.where(updated_at: (Time.now - 24.hours)..Time.now).group('url').limit(10).count
+    @urls = urls.sort_by {|key, value| value}.reverse
   end
 end
